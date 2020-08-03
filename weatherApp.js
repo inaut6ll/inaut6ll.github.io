c385038a0a12df35998cf543bac373d0f6b39bd8
milanzq309 <milanzq309@gmail.com>
2020-08-02 22:08:04 -0700

    get and console log data

--- weatherApp.js
+++ weatherApp.js
@@ -1,14 +1,23 @@
 $(document).ready(() => {
     let long;
     let lat;
+    let api;
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
             
-            const api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6c8801566f1c76a01f1321935af57876`;
+            api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6c8801566f1c76a01f1321935af57876`;
         });
+
+        fetch(api)
+            .then(response => {
+                return response.json();
+            })
+            .then(data => {
+                console.log(data);
+            })
     } else{
         $("body").html("Please allow access to location.");
     }
