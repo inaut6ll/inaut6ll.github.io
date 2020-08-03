76f3fbfa457b6e4c80e25ebe2fc85606434c1e82
milanzq309 <milanzq309@gmail.com>
2020-08-02 22:56:01 -0700

    fixed not showing on local server, also got the specific stuff i wanted from the data, also added darkmode button

--- weatherApp.js
+++ weatherApp.js
@@ -1,24 +1,46 @@
 $(document).ready(() => {
     let long;
     let lat;
-    let api;
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
             
-            api = `http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6c8801566f1c76a01f1321935af57876`;
-        });
-
-        fetch(api)
+            const proxy = "https://cors-anywhere.herokuapp.com/";
+            const api = `${proxy}http://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=6c8801566f1c76a01f1321935af57876`;
+            
+            fetch(api)
             .then(response => {
                 return response.json();
             })
             .then(data => {
                 console.log(data);
-            })
+
+                const temp = data.main.temp;
+                const info = data.weather.description;
+                const city = data.name;
+                const country = data.sys.country;
+            });
+        });
     } else{
         $("body").html("Please allow access to location.");
     }
+
+    let mode = "light";
+    $("#mode").on("click", () => {
+        if(mode === "light"){
+            mode = "dark";
+            $("#mode").css("color", "rgb(59, 59, 59)");
+            $("#mode").css("background-color", "white");
+            $("body").css("color", "white");
+            $("body").css("background-color", "rgb(20, 20, 20)");
+        }else {
+            mode = "light";
+            $("#mode").css("color", "white");
+            $("#mode").css("background-color", "rgb(59, 59, 59)");
+            $("body").css("color", "rgb(59, 59, 59)");
+            $("body").css("background-color", "white");
+        }
+    });
 });
