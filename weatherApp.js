0737153cffa08bae862db2ffd8037cda24855312
milanzq309 <milanzq309@gmail.com>
2020-08-03 01:31:28 -0700

    made it so that u can change the unit of temp by clicking on it

--- weatherApp.js
+++ weatherApp.js
@@ -1,8 +1,8 @@
 $(document).ready(() => {
     let long;
     let lat;
-    let tempF = 0;
-    let tempC = 0;
+    let tempF = 18032;
+    let tempC = 10000;
     let info = "";
     let city = "";
     let country = "";
@@ -12,41 +12,69 @@ $(document).ready(() => {
             long = position.coords.longitude;
             lat = position.coords.latitude;
             
-            const proxy = "https://cors-anywhere.herokuapp.com/";
-            const api = `${proxy}http://api.openweathermap.org/data/2.5/weather?lat=70&lon=37&appid=6c8801566f1c76a01f1321935af57876`;
+            //const proxy = "https://cors-anywhere.herokuapp.com/";
+            //const api = `${proxy}http://api.openweathermap.org/data/2.5/weather?lat=70&lon=37&appid=6c8801566f1c76a01f1321935af57876`;
             
             fetch(api)
                 .then(response => {
                     return response.json();
                 })
                 .then(data => {
-                    console.log(data);
                     $("#note").fadeOut();
+                    console.log(data);
+
+                    //convert kelvins to celsius
                     tempC = parseInt(data.main.temp) - 273;
                     tempF = Math.round((tempC * 9 / 5) + 32);
+                    city = data.name;
+                    country = data.sys.country;
+
+                    //capitalize first word of descrip
                     info = data.weather[0].description;
                     let a = info.substring(0, 1).toUpperCase();
                     let b = info.substring(1, info.length);
                     info = a + b;
-                    city = data.name;
-                    country = data.sys.country;
 
-                    console.log(temp);
-                    console.log(info);
-                    console.log(city);
-                    console.log(country);
+                    //update data
                     $("#info").html(info);
                     $("#deg").html(tempC);
 
+                    //change font size if too many letters
                     if(city.length + country.length > 20){
                         $("#loc").css("font-size", "50px");
                     }else {
                         $("#loc").css("font-size", "80px");
                     }
                     $("#loc").html(city + " / " + country);
+
+                    console.log(tempC);
+                    console.log(info);
+                    console.log(city);
+                    console.log(country);
+
+                    //see if it's night
+                    let night = false;
+                    let d = new Date();
+                    let n = d.getHours();
+                    if(n >= 20 || n <=4){
+                        night = true;
+                    }
                 });
         });
     }
+
+    let units = "c";
+    $("#temp").on("click", () => {
+        if(units === "c"){
+            units = "f";
+            $("#deg").html(tempF);
+            $("#unit").html("°F");
+        }else{
+            units = "c";
+            $("#deg").html(tempC);
+            $("#unit").html("°C");
+        }
+    });
 
     let mode = "light";
     $("#mode").on("click", () => {
@@ -64,4 +92,5 @@ $(document).ready(() => {
             $("body").css("background-color", "white");
         }
     });
+
 });
