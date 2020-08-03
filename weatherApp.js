0f126c32e879f799bb3992c9208968396b96c8b7
milanzq309 <milanzq309@gmail.com>
2020-08-03 02:27:06 -0700

    added and completed function for updating icon

--- weatherApp.js
+++ weatherApp.js
@@ -4,8 +4,56 @@ $(document).ready(() => {
     let tempF = 18032;
     let tempC = 10000;
     let info = "";
+    let infoDis = "";
     let city = "";
     let country = "";
+    let windSpeed = 0;
+
+    //change icon based on descrip
+    function updateIcon() {
+        //see if it's night
+        let night = false;
+        let d = new Date();
+        let n = d.getHours();
+        if(n >= 20 || n <=4){
+            night = true;
+        }
+        let icon = $("i");
+        icon.removeClass("fa-poo-storm");
+        if((info.includes("snow") && info.includes("storm")) || info.includes("hail") || info.includes("ice") || info.includes("icy")){
+            icon.addClass("fa-cloud-meatball");
+        }else if(info.includes("snow")){
+            icon.addClass("fa-snowflake");
+        }else if(info.includes("lightning") || info.includes("storm") || info.includes("thunder")){
+            icon.addClass("fa-bolt");
+        }else if(night && (info.includes("rain") || info.includes("sleet") || info.includes("hail"))){
+            icon.addClass("fa-cloud-moon-rain");
+        }else if(night && info.includes("cloud")){
+            icon.addClass("fa-cloud-moon");
+        }else if(night && info.includes("clear")){
+            icon.addClass("fa-moon");
+        }else if(info.includes("sun") && info.includes("rain")){
+            icon.addClass("fa-cloud-sun-rain");
+        }else if(info.includes("heavy") && info.includes("rain")){
+            icon.addClass("fa-cloud-showers-heavy");
+        }else if(info.includes("rain")){
+            icon.addClass("fa-cloud-rain");
+        }else if(windSpeed >= 20){
+            icon.addClass("fa-wind");
+        }else if(info.includes("sunny")){
+            icon.addClass("fa-sun");
+        }else if(info.includes("broken") || info.includes("scatter")){
+            icon.addClass("fa-cloud-sun");
+        }else if(info.includes("cloud")){
+            icon.addClass("fa-cloud");
+        }else if(info.includes("clear")){
+            icon.addClass("fa-parachute-box");
+        }else if(night){
+            icon.addClass("fa-moon");
+        }else{
+            icon.addClass("fa-cloud-sun");
+        }
+    };
 
     if (navigator.geolocation){
         navigator.geolocation.getCurrentPosition(position => {
@@ -26,18 +74,21 @@ $(document).ready(() => {
                     //convert kelvins to celsius
                     tempC = parseInt(data.main.temp) - 273;
                     tempF = Math.round((tempC * 9 / 5) + 32);
+
                     city = data.name;
                     country = data.sys.country;
+                    windSpeed = data.wind.speed;
 
                     //capitalize first word of descrip
                     info = data.weather[0].description;
                     let a = info.substring(0, 1).toUpperCase();
                     let b = info.substring(1, info.length);
-                    info = a + b;
+                    infoDis = a + b;
 
                     //update data
-                    $("#info").html(info);
+                    $("#info").html(infoDis);
                     $("#deg").html(tempC);
+                    updateIcon();
 
                     //change font size if too many letters
                     if(city.length + country.length > 20){
@@ -51,14 +102,6 @@ $(document).ready(() => {
                     console.log(info);
                     console.log(city);
                     console.log(country);
-
-                    //see if it's night
-                    let night = false;
-                    let d = new Date();
-                    let n = d.getHours();
-                    if(n >= 20 || n <=4){
-                        night = true;
-                    }
                 });
         });
     }
