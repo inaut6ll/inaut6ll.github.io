8b3af3424b1640afe65797604394cdfbade87afc
Lilian Zhao <milanzq309@gmail.com>
2020-12-29 05:02:29 -0800

    added cube animation and up btn

--- indexApp.js
+++ indexApp.js
@@ -1,1 +1,38 @@
-//alert("Note: This page is currently under reconstruction, which is why it likely looks terrible at the moment.");
+//cube animation
+let isSpinning = true;
+
+const illo = new Zdog.Illustration({
+    element: '.canvas',
+    zoom: .6,
+    dragRotate: true,
+    onDragStart: function() {
+        isSpinning = false;
+    }
+})
+
+const cube = new Zdog.Box({
+    addTo: illo,
+    width: 50,
+    height: 50, 
+    depth: 50,
+    stroke: false,
+    leftFace: "rgb(200,200,200)",
+    rightFace: "rgb(0,0,0)",
+    topFace: "rgb(255, 101, 101)",
+    bottomFace: "rgb(151, 243, 255)",
+})
+
+illo.updateRenderGraph();
+
+function animate() {
+    if (isSpinning) {
+        illo.rotate.y += 0.01;
+        illo.rotate.x += 0.01;
+    }
+    illo.updateRenderGraph();
+    requestAnimationFrame(animate);
+}
+
+animate();
+
+
