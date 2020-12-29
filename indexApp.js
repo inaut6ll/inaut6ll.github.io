a7d92807622e5ca58693d38d1ae644d4b14dfd08
Lilian Zhao <milanzq309@gmail.com>
2020-12-29 06:33:44 -0800

    added icons

--- indexApp.js
+++ indexApp.js
@@ -36,3 +36,18 @@ function animate() {
 animate();
 
 
+//animate mouse parallax
+const mouseParallax = (element, xDistance, yDistance, speed) => {
+    let x = (window.innerWidth - xDistance * speed) / 100;
+    let y = (window.innerHeight - yDistance * speed) / 100;
+
+    element.style.transform = `translate(${-x}px,${y}px)`;
+}
+
+document.addEventListener("mousemove", e=> {
+    document.querySelectorAll(".frag").forEach(frag => {
+        const speed = frag.getAttribute("data-speed");
+
+        mouseParallax(frag, e.pageX, e.pageY, speed);
+    })
+})
