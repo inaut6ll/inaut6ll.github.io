a573b1c3647c500476a79ae435847e6ca7b6bfbf
Lilian Zhao <milanzq309@gmail.com>
2020-12-30 02:09:54 -0800

    added function to contact and other stuff

--- indexApp.js
+++ indexApp.js
@@ -1,12 +1,30 @@
-//onclick functions for circles
+//contact
+document.querySelector("#contact-btn").addEventListener("click", (e) => {
+    console.log("test pasdfe");
+    document.querySelector("#c3").className = "circle-hover";
+    document.querySelector("#contact3").style.display = "block";
+});
 
+
+//onclick functions for circles
 let elements = [...document.querySelectorAll(".circle")];
 elements.forEach(element => {
     element.addEventListener("click", (e) => {
-        if (e.target.className == "circle-hover"){
-            e.target.className = "circle";
-        } else if (e.target.className.includes("circle")){
-            e.target.className = "circle-hover";
+        let a = e.target;
+        if (a.className == "circle-hover"){
+            a.className = "circle";
+            if (a.id == "c2") {
+                document.querySelector("#contact2").style.display = "none";
+            } else {
+                document.querySelector("#contact3").style.display = "none";
+            }
+        } else if (a.className.includes("circle")){
+            a.className = "circle-hover";
+            if (a.id == "c2") {
+                document.querySelector("#contact2").style.display = "block";
+            } else {
+                document.querySelector("#contact3").style.display = "block";
+            }
         }
     });
 });
