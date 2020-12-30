3a4abc1ef27b7f157d00cde01481fd85152f7269
Lilian Zhao <milanzq309@gmail.com>
2020-12-30 04:52:17 -0800

    fixed responsiveness stufsfjeoi

--- indexApp.js
+++ indexApp.js
@@ -15,16 +15,24 @@ document.addEventListener("DOMContentLoaded", () => {
             if (a.className == "circle-hover"){
                 a.className = "circle";
                 if (a.id == "c2") {
+                    a.style.borderColor = "black";
                     document.querySelector("#contact2").style.display = "none";
                 } else if (a.id == "c3") {
+                    a.style.borderColor = "tomato";
                     document.querySelector("#contact3").style.display = "none";
+                } else{
+                    a.style.borderColor = "black";
                 }
             } else if (a.className.includes("circle")){
                 a.className = "circle-hover";
                 if (a.id == "c2") {
+                    a.style.borderColor = "rgb(151, 243, 255)";
                     document.querySelector("#contact2").style.display = "flex";
                 } else if (a.id == "c3") {
+                    a.style.borderColor = "black";
                     document.querySelector("#contact3").style.display = "grid";
+                } else {
+                    a.style.borderColor = "tomato";
                 }
             }
         });
