93c0a9f09f6fe1cc149e97594a54abb76e242aeb
Lilian Zhao <milanzq309@gmail.com>
2021-01-01 14:09:01 -0800

    created project container for 2 more projects

--- indexApp.js
+++ indexApp.js
@@ -49,41 +49,119 @@ document.addEventListener("DOMContentLoaded", () => {
         });
     });
 
-    //add functionality to image (takes you to the project) 
+    //add functionality to weather image 
+    document.querySelector("#weather-hover").addEventListener("click", () => {
+        window.open("https://lilianzlettuce.github.io/weather", "_blank");
+    });
+
+    //add functionality to weather right arrow
+    let currentWeatherDot = 1;
+    document.querySelector("#weather-icon").addEventListener("click", () => {
+        let dot1 = document.querySelector("#weather-dot1");
+        let dot2 = document.querySelector("#weather-dot2");
+        let dot3 = document.querySelector("#weather-dot3");
+        let pic2 = document.querySelector("#weather2");
+        let pic3 = document.querySelector("#weather3");
+        if(currentWeatherDot === 1){
+            currentWeatherDot = 2;
+            dot1.style.backgroundColor = "white";
+            dot1.style.border = "1px solid black";
+            dot2.style.backgroundColor = "rgb(71, 166, 255)";
+            dot2.style.border = "none";
+            pic2.style.opacity = "100%";
+        } else if(currentWeatherDot === 2){
+            currentWeatherDot = 3;
+            dot2.style.backgroundColor = "white";
+            dot2.style.border = "1px solid black";
+            dot3.style.backgroundColor = "rgb(71, 166, 255)";
+            dot3.style.border = "none";
+            pic2.style.opacity = "0%";
+            pic3.style.opacity = "100%";
+        } else{
+            currentWeatherDot = 1;
+            dot3.style.backgroundColor = "white";
+            dot3.style.border = "1px solid black";
+            dot1.style.backgroundColor = "rgb(71, 166, 255)";
+            dot1.style.border = "none";
+            pic3.style.opacity = "0%";
+        }
+    });
+
+    //add functionality to 2048 image 
     document.querySelector("#two-hover").addEventListener("click", () => {
         window.open("https://lilianzlettuce.github.io/2048", "_blank");
     });
 
-    //add functionality to right arrow (changes images when clicked)
-    let currentDot = 1;
+    //add functionality to 2048 right arrow
+    let currentTwoDot = 1;
     document.querySelector("#two-icon").addEventListener("click", () => {
         let dot1 = document.querySelector("#two-dot1");
         let dot2 = document.querySelector("#two-dot2");
         let dot3 = document.querySelector("#two-dot3");
         let pic2 = document.querySelector("#two2");
         let pic3 = document.querySelector("#two3");
-        if(currentDot === 1){
-            currentDot = 2;
+        if(currentTwoDot === 1){
+            currentTwoDot = 2;
             dot1.style.backgroundColor = "white";
             dot1.style.border = "1px solid black";
-            dot2.style.backgroundColor = "tomato";
+            dot2.style.backgroundColor = "orange";
             dot2.style.border = "none";
             pic2.style.opacity = "100%";
-        } else if(currentDot === 2){
-            currentDot = 3;
+        } else if(currentTwoDot === 2){
+            currentTwoDot = 3;
             dot2.style.backgroundColor = "white";
             dot2.style.border = "1px solid black";
-            dot3.style.backgroundColor = "tomato";
+            dot3.style.backgroundColor = "orange";
             dot3.style.border = "none";
             pic2.style.opacity = "0%";
             pic3.style.opacity = "100%";
         } else{
-            currentDot = 1;
+            currentTwoDot = 1;
             dot3.style.backgroundColor = "white";
             dot3.style.border = "1px solid black";
-            dot1.style.backgroundColor = "tomato";
+            dot1.style.backgroundColor = "orange";
             dot1.style.border = "none";
             pic3.style.opacity = "0%";
         }
     });
+
+    //add functionality to tetris image 
+    document.querySelector("#tetris-hover").addEventListener("click", () => {
+        window.open("https://lilianzlettuce.github.io/tetris", "_blank");
+    });
+
+    //add functionality to tetris right arrow
+    let currenttetrisDot = 1;
+    document.querySelector("#tetris-icon").addEventListener("click", () => {
+        let dot1 = document.querySelector("#tetris-dot1");
+        let dot2 = document.querySelector("#tetris-dot2");
+        let dot3 = document.querySelector("#tetris-dot3");
+        let pic2 = document.querySelector("#tetris2");
+        let pic3 = document.querySelector("#tetris3");
+        if(currenttetrisDot === 1){
+            currenttetrisDot = 2;
+            dot1.style.backgroundColor = "white";
+            dot1.style.border = "1px solid black";
+            dot2.style.backgroundColor = "rgb(103, 219, 151)";
+            dot2.style.border = "none";
+            pic2.style.opacity = "100%";
+        } else if(currenttetrisDot === 2){
+            currenttetrisDot = 3;
+            dot2.style.backgroundColor = "white";
+            dot2.style.border = "1px solid black";
+            dot3.style.backgroundColor = "rgb(103, 219, 151)";
+            dot3.style.border = "none";
+            pic2.style.opacity = "0%";
+            pic3.style.opacity = "100%";
+        } else{
+            currenttetrisDot = 1;
+            dot3.style.backgroundColor = "white";
+            dot3.style.border = "1px solid black";
+            dot1.style.backgroundColor = "rgb(103, 219, 151)";
+            dot1.style.border = "none";
+            pic3.style.opacity = "0%";
+        }
+    });
+
+
 });
